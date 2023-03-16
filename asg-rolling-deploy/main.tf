52821bcf1cf0ea05ddad93c8b7429448a0bd6bdf
Javel Wilson <javelawilson@gmail.com>
2023-03-15 23:47:02 -0500

    Create launch configuration resource in asg-rolling-deploy module

--- asg-rolling-deploy/main.tf
+++ asg-rolling-deploy/main.tf
@@ -8,3 +8,16 @@ terraform {
     }
   }
 }
+
+
+resource "aws_launch_configuration" "example" {
+  image_id        = var.ami
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.instance.id]
+  user_data       = var.user_data
+
+  # Required when using a launch configuration with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
